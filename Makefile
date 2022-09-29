80c75cb0a96c1e094daead72efed5b2eb49f963e
Павел Шорин <pavelsorin@MacBook-Air-Pavel.local>
2022-09-29 20:53:27 +0300

    Environment setup, Step 2

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+	npm ci
+
+brain-games:
+	node bin/brain-games.js
+
+publish:
+	npm publish --dry-run
---
+++ bin/brain-games.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log("node bin/brain-games.js");
+console.log("Welcome to the Brain Games!");
