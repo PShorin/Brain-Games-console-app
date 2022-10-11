afb90eeb077941dbc62528e2093dd967d23a0460
Павел Шорин <pavelsorin@MacBook-Air-Pavel.local>
2022-10-11 18:38:08 +0200

    Made 2 game Calc, add index.js, Step 6

--- Makefile
+++ Makefile
@@ -9,3 +9,9 @@ publish:
 
 lint:
 	npx eslint .
+
+brain-even:
+	node bin/brain-even.js
+
+brain-calc:
+	node bin/brain-calc.js
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const getRandomNumber = (max, min) => Math.floor(Math.random() * (max - min) + min);
+
+export const isEven = (number) => number % 2 === 0;
