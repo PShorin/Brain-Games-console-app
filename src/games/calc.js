af6e8d5714bb76b1e6728128487f29bd85a172d1
Павел Шорин <pavelsorin@MacBook-Air-Pavel.local>
2022-10-17 17:56:36 +0200

    update calc.js, new function

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,25 +3,29 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What is the result of the expression?';
 
+const calculate = (x, y, sign) => {
+  switch (sign) {
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
+    default:
+      return null;
+  }
+};
+
 const getQuestionAndAnswer = () => {
   const mathSign = ['+', '-', '*'];
   const mathSignInTask = mathSign[getRandomNumber(0, 3)];
 
-  const firstNumberForOperation = getRandomNumber(0, 100);
-  const secondNumberForOperation = getRandomNumber(0, 50);
-  const extraNumberForDivide = getRandomNumber(2, 5);
-  if (mathSignInTask === '+') {
-    const question = `${firstNumberForOperation} + ${secondNumberForOperation}`;
-    const correctAnswer = firstNumberForOperation + secondNumberForOperation;
-    return [question, correctAnswer];
-  } if (mathSignInTask === '-') {
-    const question = `${firstNumberForOperation} - ${secondNumberForOperation}`;
-    const correctAnswer = firstNumberForOperation - secondNumberForOperation;
-    return [question, correctAnswer];
-  }
-  const question = `${secondNumberForOperation} * ${extraNumberForDivide}`;
-  const correctAnswer = secondNumberForOperation * extraNumberForDivide;
-  return [question, correctAnswer];
+  const firstNumber = getRandomNumber(0, 50);
+  const secondNumber = getRandomNumber(0, 20);
+
+  const correctAnswer = calculate(firstNumber, secondNumber, mathSignInTask);
+  const question = `${firstNumber} ${mathSignInTask} ${secondNumber}`;
+  return [question, correctAnswer.toString()];
 };
 
 const runGameCalc = () => {
