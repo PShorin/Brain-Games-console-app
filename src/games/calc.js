ab1d197dc5d4acc4618e7f7870540dc713638832
Павел Шорин <pavelsorin@MacBook-Air-Pavel.local>
2022-10-17 16:06:15 +0200

    fix calc.js, made + - *

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,15 +4,17 @@ import getRandomNumber from '../utils.js';
 const description = 'What is the result of the expression?';
 
 const getQuestionAndAnswer = () => {
-  const numberOfMathOperation = getRandomNumber(0, 3);
+  const mathSign = ['+', '-', '*'];
+  const mathSignInTask = mathSign[getRandomNumber(0, 3)];
+
   const number1 = getRandomNumber(0, 100);
   const number2 = getRandomNumber(0, 50);
   const number3 = getRandomNumber(2, 5);
-  if (numberOfMathOperation === 0) {
+  if (mathSignInTask === '+') {
     const question = `${number1} + ${number2}`;
     const correctAnswer = number1 + number2;
     return [question, correctAnswer];
-  } if (numberOfMathOperation === 1) {
+  } if (mathSignInTask === '-') {
     const question = `${number1} - ${number2}`;
     const correctAnswer = number1 - number2;
     return [question, correctAnswer];
