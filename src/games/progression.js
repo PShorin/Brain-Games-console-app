483c3bd828aa079470d9db4e4109fd29c0948778
Павел Шорин <pavelsorin@MacBook-Air-Pavel.local>
2022-10-17 18:46:43 +0200

    update progression.js, new function

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,20 +3,27 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What number is missing in the progression?';
 
+const makeProgression = (number, step, length) => {
+  const defaultArray = [number];
+  let lastNumber = number;
+  for (let i = 0; i <= length - 1; i += 1) {
+    defaultArray.push(lastNumber + step);
+    lastNumber += step;
+  }
+  return defaultArray;
+};
+
 const getQuestionAndAnswer = () => {
   const firstNumberOfProgression = getRandomNumber(1, 50);
   const progressionStep = getRandomNumber(1, 15);
   const progressionLength = getRandomNumber(5, 10);
-  const defaultArray = [firstNumberOfProgression];
-  let lastNumber = firstNumberOfProgression;
-  for (let i = 0; i <= progressionLength - 1; i += 1) {
-    defaultArray.push(lastNumber + progressionStep);
-    lastNumber += progressionStep;
-  }
-  const whichNumberUnknown = getRandomNumber(1, defaultArray.length);
-  const correctAnswer = defaultArray[whichNumberUnknown - 1];
-  defaultArray[whichNumberUnknown - 1] = '..';
-  const question = defaultArray.join(' ');
+
+  const progression = makeProgression(firstNumberOfProgression, progressionStep, progressionLength);
+
+  const whichNumberUnknown = getRandomNumber(1, progression.length - 1);
+  const correctAnswer = progression[whichNumberUnknown];
+  progression[whichNumberUnknown] = '..';
+  const question = progression.join(' ');
 
   return [question, correctAnswer];
 };
