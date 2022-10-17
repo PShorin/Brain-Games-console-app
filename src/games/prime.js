fc4543f5fd418822421bf9d7645d4ca78fdb76b4
Павел Шорин <pavelsorin@MacBook-Air-Pavel.local>
2022-10-17 18:21:08 +0200

    update prime.js, new function

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,19 +3,19 @@ import getRandomNumber from '../utils.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const getQuestionAndAnswer = () => {
-  const question = getRandomNumber(1, 40);
-  const isPrime = () => {
-    if (question < 2) return false;
-    let divider = 2;
-    while (divider <= question / 2) {
-      if (question % divider === 0) return false;
-      divider += 1;
-    }
-    return true;
-  };
-  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+const isPrime = (number) => {
+  if (number < 2) return false;
+  let divider = 2;
+  while (divider <= number / 2) {
+    if (number % divider === 0) return false;
+    divider += 1;
+  }
+  return true;
+};
 
+const getQuestionAndAnswer = () => {
+  const question = getRandomNumber(2, 40);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
